fe6d4c6834145fe0f9fe43e62c79860dbe37bb36
deepjoy9 <49182381+deepjoy9@users.noreply.github.com>
2022-06-22 21:55:49 +0530

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -57,11 +57,11 @@ def check_win(board, marker):
 
 def dispboard(board):
     clear()
-    print(f'  {board[7]} | {board[8]} | {board[9]}')
+    print(f'  {board[1]} | {board[2]} | {board[3]}')
     print('-------------')
     print(f'  {board[4]} | {board[5]} | {board[6]}')
     print('-------------')
-    print(f'  {board[1]} | {board[2]} | {board[3]}')
+    print(f'  {board[7]} | {board[8]} | {board[9]}')
 
 
 def replay():
